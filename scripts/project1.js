4bd406235dc3a2a4b05be46c9113db1048c57a41
Ana Liebert <analiebert19@gmail.com>
2022-01-10 08:38:43 -0500

    level 2 included

--- scripts/project1.js
+++ scripts/project1.js
@@ -6,13 +6,28 @@ let seconds = 0;
 let isMatch = 0;
 let totalClicks = 0;
 let sumArray = [];
+let container = document.querySelector('.container');
+let container2 = document.querySelector('.container2');
+let $button = $('#duplicate');
+let button = document.getElementById('#duplicate');
+let $levelNumber = $('#level').text();
+let $levelMessage = $('#level');
+
+
+console.log($levelNumber);
+console.log($levelNumber.includes('level'));
+console.log($levelNumber.includes('2'));
+
+let $nextLevel = $('.container2');
+$nextLevel.hide();
+
 
 
 // Shuffles cards ------->
 
 $('.timerContainer').hide();
 
-let container = document.querySelector('.container');
+
 
 for (let i = 0; i <= 12; i++) {
     container.appendChild(container.children[Math.floor(Math.random() * i)]);
@@ -33,6 +48,7 @@ interval = window.setInterval(timer, 1000);
 
 // Show images and check for match ------->
 function displayImage() {
+
   this.classList.add('flip');
   totalClicks ++
   console.log(totalClicks);
@@ -45,6 +61,8 @@ function displayImage() {
       countClicks = 0
       choiceTwo = this;
     console.log(choiceOne.dataset.name, choiceTwo.dataset.name)
+         
+    
         if (choiceOne.dataset.name == choiceTwo.dataset.name) {
             isMatch ++
             sumArray.push(totalClicks);
@@ -59,6 +77,13 @@ function displayImage() {
                     $('#clickTally').append(totalClicks); 
                 }
 
+                if (isMatch == 12) {
+                alert('level 2 complete');
+                window.clearInterval(interval);
+                    $('.timerContainer').slideDown();
+                    sumClicks = sumArray.slice(-1)
+                    $('#clickTally').append(totalClicks); 
+            }
             } else{
             setTimeout(() => {
                 choiceOne.classList.remove('flip');
@@ -72,29 +97,29 @@ cards.forEach(card => card.addEventListener('click', displayImage));
 
 
 
-// let $button = $('#duplicate')
-// $button.on('click', duplicate);
 
-// let $containerDuplicate = $('.container')
-
-// function duplicate() {
-//     newBoard = $containerDuplicate.clone(true, true);
-//     $(newBoard).insertAfter('.container:last');
-//     console.log(newBoard);
-// }
+$button.on('click', duplicate);
 
 
-// let clone = ''
-// let containerDuplicate = document.querySelector('.container');
-// let duplicateButton = document.querySelector('#duplicate');
+function duplicate() {
+    
+    $nextLevel.show();
+    $levelMessage.text('Level 2');
+    container.classList.add('levelUp');
+    container2.classList.add('levelUp');
+    
+
+    for (let i = 0; i <= 12; i++) {
+
+                container.appendChild(container2.children[Math.floor(Math.random() * i)]);
+                container2.appendChild(container.children[Math.floor(Math.random() * i)]);
+    }
 
 
-// duplicateButton.addEventListener('click', function() {
-//     let clone = containerDuplicate.cloneNode(true);
-//     containerDuplicate.after(clone);
-//     containerDuplicate.after(clone);
+}
 
-//     cards.forEach(card => card.addEventListener('click', displayImage));
 
-// })
+
+//LEVEL UP 
+//things im doing- adding different class list to all second container, duplicating the click event for those cards, then creating a new function looking for 12 matches
 
